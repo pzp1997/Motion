54fe38219d58db2554351b35f9c8bc5904277204
Palmer <pzpaul2002@yahoo.com>
2016-01-05 21:55:20 -0500

    finalize hungarian algorithm stuff

--- BgSub.py
+++ BgSub.py
@@ -1,17 +1,30 @@
 import numpy as np
 import cv2
+from ObjectIdAssignment import find_ids_of_objects
 
 # Constants
 MIN_AREA = 1600
-MORPH_SIZE = 20
+#MAX_AREA = 3600
+MORPH_SIZE = 30
 
 # Globals
 cam = cv2.VideoCapture(1)
 frame = cam.read()[1]
+centroids = []
+
+
+def calc_centroid(contour):
+    """Returns a tuple representing the centroid of ``contour``"""
+    moments = cv2.moments(contour)
+    cx = int(moments['m10'] / moments['m00'])
+    cy = int(moments['m01'] / moments['m00'])
+    return cx, cy
 
 while True:
     # Grab new frame
     prev_frame, frame = frame, cam.read()[1]
+
+    frame = cv2.GaussianBlur(frame, (5,5), 0)
 
     # Calculate the difference between current frame and previous frame
     diff = cv2.absdiff(frame, prev_frame)
@@ -19,11 +32,6 @@ while True:
     # Create binary image of difference
     thresh = cv2.threshold(cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY),
                            1, 255, cv2.THRESH_BINARY)[1]
-    
-##    thresh = cv2.erode(thresh,
-##                       cv2.getStructuringElement(cv2.MORPH_RECT,(20,20)))
-##    thresh = cv2.dilate(thresh,
-##                        cv2.getStructuringElement(cv2.MORPH_RECT,(5,5)))
 
     # "Open" threshold image to remove noise                     
     thresh = cv2.morphologyEx(thresh,
@@ -35,17 +43,26 @@ while True:
     contours = cv2.findContours(thresh,
                                 cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)[0]
-
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > MIN_AREA]
+    
+    centroids, predicts = map(calc_centroid, contours), centroids
+    
+    ordered_objs = find_ids_of_objects(centroids, predicts)
+    
+    #cv2.drawContours(frame, contours, -1, (0,0,255), thickness=2)
+    
+    for i, pos in enumerate(ordered_objs, start=1):
+        cv2.putText(frame, str(i), pos,
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), thickness=2)
+        
+    
     # Draw bounding boxes around the contours
-    contour_count = 0
-    for contour in contours:
-        if cv2.contourArea(contour) > MIN_AREA:
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
-            contour_count += 1
-    print contour_count
+##    for contour in contours:
+##        if cv2.contourArea(contour) > MIN_AREA:
+##            centroids.append(calc_centroid(contour))
+##            x, y, w, h = cv2.boundingRect(contour)
+##            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
 
-##    cv2.drawContours(frame, contours, -1, (255,0,0), 3)
 
     # Display image with contours and threshold image
     cv2.imshow('Foreground', thresh)
--- ObjectIdAssignment.py
+++ ObjectIdAssignment.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from munkres import Munkres
+from operator import itemgetter
 
 munk = Munkres()
 
@@ -20,20 +21,30 @@ def create_distance_matrix(posit, predict):
 
 
 def dist_btwn(pt1, pt2):
+    """Returns distance between ``pt1`` and ``pt2``"""
     return ((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)**0.5
 
 
 def pad_to_square(mat, pad_val=0):
-    if mat.shape[0] > mat.shape[1]:
-        pad_width = ((0,0), (0,mat.shape[0]-mat.shape[1]))
-    else:
-        pad_width = ((0,mat.shape[1]-mat.shape[0]), (0,0))
+    """Returns a square matrix of ``mat`` padded with ``pad_val``"""
+    
+    pad_width = (((0,0), (0,mat.shape[0]-mat.shape[1]))
+                 if mat.shape[0] > mat.shape[1] else
+                 ((0,mat.shape[1]-mat.shape[0]), (0,0)))
 
     return np.pad(mat, pad_width, mode='constant', constant_values=pad_val)
 
 
 def find_ids_of_objects(positions, predictions):
+    """Returns the new indices of each object based on Hungarian Algorithm"""
+    if len(positions) < 1 or len(predictions) < 1:
+        return []
+    
     distance_matrix = create_distance_matrix(positions, predictions)
     distance_matrix = pad_to_square(distance_matrix, np.amax(distance_matrix))
-    indices = m.compute(distance_matrix)
-    return indices
+    
+    indices = munk.compute(distance_matrix)
+    indices = [idx[1] for idx in indices if idx[1] < len(predictions)]
+
+    new_order = itemgetter(*indices)(predictions)
+    return new_order if len(indices) > 1 else (new_order,)
